04d2b0b56108f21ac8286216a319d10b38c6f060
seto.sun <seto.sun@sinodynamic.com>
2017-05-04 16:40:51 +0800

    Modify

--- demo-mall/app/App.vue
+++ demo-mall/app/App.vue
@@ -1,33 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs2222</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-    <span v-bind:title="message">
-        鼠标悬停几秒钟查看此处动态绑定的提示信息！
-    </span>
-    <ol>
-       <li v-for="todo in todoList">
-          {{ todo.text }}
-       </li>
-    </ol>
-    <p>{{ message }}</p>
-    <button v-on:click="reverseMessage">逆转消息</button>
-
-    <div><router-link to="/first">第一页</router-link></div>
+    <div v-for="router in routers"><router-link to="/first">第一页</router-link></div>
     <div><router-link to="/second">第二页</router-link></div>
     <router-view class="view"></router-view>
   </div>
@@ -37,13 +11,7 @@
 export default {
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      message: '页面加载于 ' + new Date(),
-      todoList: [
-             { text: '学习 JavaScript' },
-              { text: '学习 Vue' },
-              { text: '整个牛项目' }
-            ]
+      routers:[]
     }
   },
   methods: {
